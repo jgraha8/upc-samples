b000256c33e31822cbecfe1ceb08b7967094f350
Jason Graham <jgraha8@gmail.com>
2016-05-02 15:17:17 -0400

    Updated the code to continue until convergence; added CFL based timestep.

--- cond.c
+++ cond.c
@@ -7,18 +7,21 @@
 #include <sys/time.h>
 #include <stdbool.h>
 
-#define NX 256
-#define NY 256
+#define NX 512
+#define NY 512
 
 #define DX 0.05
 #define DY 0.05
 #define NSTEPS 100000
 
 #define ALPHA 1.0
-#define DT 0.0001
+
+#define CFL 0.95
 
 #define NWORKSP 3
-#define OUTPUT_FREQ 10000
+#define OUTPUT_FREQ 5.0
+
+#define TOL 1.0e-6
 
 static bool buffer_shared = false;
 
@@ -53,8 +56,6 @@ void uclock_split_stop( uclock_t *this_ )
 	this_->time += uclock_time(this_);
 	
 }
-
-
 
 typedef struct {
 	int dims[2];
@@ -91,6 +92,12 @@ void field_dtor( field_t *this_ )
 	upc_free(this_->sh_v);
 	this_->size = 0;
 	memset(this_->dims, 0, sizeof(this_->dims));
+}
+
+inline void field_copy( field_t *this_, const field_t *f_ )
+{
+	assert(this_->size == f_->size );
+	memcpy( this_->v, f_->v, this_->size*sizeof(*this_->v));
 }
 
 inline size_t field_eval_index(const field_t *this_, int i_, int j_ )
@@ -158,6 +165,11 @@ void pfield_set( pfield_t *this_, double v_ )
 	upc_barrier;	
 }
 
+inline void pfield_copy( pfield_t *this_, const pfield_t *f_ )
+{
+	field_copy(this_->field, f_->field );
+}
+
 // Returns the local block vector index for a given thread reference
 inline size_t pfield_local_index_th(const pfield_t *this_, int th_id_, int i_, int j_ )
 {
@@ -170,27 +182,95 @@ inline void pfield_eval_ij_th(const pfield_t *this_, int th_id_, int i_th_, int 
 	*j_ = j_th_;
 }
 
-/* inline size_t pfield_eval_index(const pfield_t *this_, int i_, int j_ ) */
-/* { */
-/* 	return (size_t)i_*this_->dims[1] + j_; */
-/* } */
+double field_get_max( const field_t *this_ )
+{
+	double v_max = -1.0e32;
+	double *v     = this_->v;
+	double *v_end = v + this_->size;
 
-/* inline size_t pfield_eval_shth_index(const pfield_t *this_, int th_id_, int i_, int j_ ) */
-/* { */
-/* 	return eval_sh_field_index( this_->sh_field[th_id_], i_, j_ ); */
-/* } */
+	while( v < v_end ) {
+		if( v_max < *v ) v_max = *v;
+		v++;
+	}
+	return v_max;
+}
 
-/* inline size_t pfield_eval_index_th(const pfield_t *this_, int i_, int j_ ) */
-/* { */
-/* 	return (size_t)(i_ + this_->offset[0])*this_->dims[1] + j_; */
-/* } */
+double field_get_min( const field_t *this_ )
+{
+	double v_min = 1.0e32;
+	double *v     = this_->v;
+	double *v_end = v + this_->size;
 
-/* inline size_t pfield_eval_index_shth(const pfield_t *this_, int th_id_, int i_, int j_ ) */
-/* { */
-/* 	return (size_t)(i_ + this_->sh_offset[th_id_][0])*this_->dims[1] + j_; */
-/* } */
+	while( v < v_end ) {
+		if( *v < v_min ) v_min = *v;
+		v++;
+	}
+	return v_min;
+}
 
+shared double sh_val; // Shared value with affinity thread=0
+upc_lock_t *lock = NULL;
 
+void init_locks()
+{
+	lock = upc_all_lock_alloc();
+}
+
+void fini_locks()
+{
+	if( MYTHREAD == 0 ) {
+		if( lock ) upc_lock_free(lock);
+	}
+}
+double allreduce_max( double v_, int th_id_ )
+{
+	assert( lock );
+
+	if( th_id_ == 0 ) {
+		sh_val = -1.0e32;
+	}
+	upc_lock( lock );
+	if( sh_val < v_ ) {
+		sh_val = v_;
+	}
+	upc_unlock( lock );
+
+	upc_barrier;
+	
+	return sh_val;
+}
+
+double allreduce_min( double v_, int th_id_ )
+{
+	assert( lock );
+
+	if( th_id_ == 0 ) {
+		sh_val = 1.0e32;
+	}
+	upc_lock( lock );
+	if( v_ < sh_val ) {
+		sh_val = v_;
+	}
+	upc_unlock( lock );
+
+	upc_barrier;
+	
+	return sh_val;
+}
+
+inline double pfield_get_max( const pfield_t *this_ )
+{
+	double v_max = field_get_max(this_->field );
+	v_max = allreduce_max( v_max, this_->th_id );
+	return v_max;
+}
+
+inline double pfield_get_min( const pfield_t *this_ )
+{
+	double v_min = field_get_min(this_->field );
+	v_min = allreduce_min( v_min, this_->th_id );
+	return v_min;
+}
 void pfield_eval_ddx( pfield_t *this_, pfield_t *ddx_ )
 {
 	assert( this_->dims[0] == ddx_->dims[0] &&
@@ -207,6 +287,7 @@ void pfield_eval_ddx( pfield_t *this_, pfield_t *ddx_ )
 	
 	int i,j,idx;
 
+	// Interior, non-boundary (physical and inter-thread) points
 	for(i=1; i<f->dims[0]-1; i++ ) {
 		v_im1 = f->v + field_eval_index(f,i-1,0);
 		v_ip1 = f->v + field_eval_index(f,i+1,0);
@@ -215,7 +296,6 @@ void pfield_eval_ddx( pfield_t *this_, pfield_t *ddx_ )
 			dv[idx++] = 0.5*(v_ip1[j] - v_im1[j]) / DX;
 		}
 	}
-
 
 	// Left inter-thread boundary
 	if( 0 < this_->th_id ) {
@@ -434,13 +514,38 @@ void set_bc( pfield_t *T_ )
 	upc_barrier;
 }
 
+double compute_Linf_norm( const pfield_t *f_, const pfield_t *g_ )
+{
+	assert( f_->field->size == g_->field->size );
+
+	const size_t N = f_->field->size;
+	const double *f_v = f_->field->v;
+	const double *g_v = g_->field->v;
+
+	size_t i;
+	double dv;
+	double dv_max = -1.0e32;
+	
+	// Find the max difference between the two fields
+	for( i=0; i<N; i++ ) {
+		dv = fabs( f_v[i] - g_v[i] );
+		if( dv_max < dv ) dv_max = dv;
+	}
+
+	return allreduce_max( dv_max, f_->th_id );
+}
+
 int main(int argc_, char *argv_[])
 {
 
-	pfield_t T, D;
+	pfield_t T[2], D;
 	pfield_t ws[3];
 	uclock_t clock;
-	clock.time = 0.0;
+	shared[] uclock_t *sh_elapsed_clock = (shared[] uclock_t *)upc_all_alloc( 1, sizeof(uclock_t) );
+	double T_max, T_min;
+	double err=1.0;
+	
+	const double dt = pow( ( DX < DY ? DX : DY ), 2.0 ) * CFL / ALPHA;
 
 	int i,n;
 	int dims[] = { NX, NY };
@@ -456,59 +561,87 @@ int main(int argc_, char *argv_[])
 		}
 		n++;	
 	}
+
+	clock.time = 0.0;
+	if( MYTHREAD == 0 ) sh_elapsed_clock->time = 0.0;
+
+	init_locks();
 	
-	pfield_ctor(&T, MYTHREAD, THREADS, dims);
+	pfield_ctor(T, MYTHREAD, THREADS, dims);
+	pfield_ctor(T+1, MYTHREAD, THREADS, dims);	
 	pfield_ctor(&D, MYTHREAD, THREADS, dims );
 
 	for( n=0; n<NWORKSP; n++ )
 		pfield_ctor(ws+n, MYTHREAD, THREADS, dims );
 	
-	pfield_set(&T,0.0);
-	set_bc(&T);
+	pfield_set(T,0.0);
+	set_bc(T);
 
 	if( MYTHREAD == 0 ) 
-		pfield_write_file( &T, "T-init.dat" );
+		pfield_write_file( T, "T-init.dat" );
 
-	for( n=0; n<NSTEPS; n++ ) {
+	n=0;
+	err = 1.0;
+	
+	while( err > TOL ) {
+
+		n++;
 
 		upc_barrier;
-		if( MYTHREAD == 0 )
+
+		if( MYTHREAD == 0 ) {
+			uclock_start((uclock_t *)sh_elapsed_clock);
 			uclock_start( &clock );
+		}
+
+		pfield_copy( T+1, T );	
 		
-		pfield_eval_lap( &T, NWORKSP, ws, &D);
-		pfield_mul_scalar( &D, DT*ALPHA );
+		pfield_eval_lap( T, NWORKSP, ws, &D);
+		pfield_mul_scalar( &D, dt*ALPHA );
 
 		// Perform update
-		pfield_add( &T, &D );
-		set_bc(&T);
+		pfield_add( T, &D );
+		set_bc(T);
 
-		if( MYTHREAD == 0 )
+		err = compute_Linf_norm( T, T+1 );
+
+		upc_barrier;
+		if( MYTHREAD == 0 ) {
 			uclock_split_stop( &clock );
-
-		if( n % (NSTEPS/100) == 0 && MYTHREAD == 0 )  {
-			printf("Completed %6.3f%%\n", 100.0*(double)n / NSTEPS);
-			fflush(stdout);
+			uclock_split_stop( (uclock_t *)sh_elapsed_clock );			
 		}
-		if( n % OUTPUT_FREQ == 0 ) {
-			if( MYTHREAD == 0 ) {
-				char fname[64];
-				sprintf(fname,"T-%d.dat", n);
-				pfield_write_file( &T, fname);
+		upc_barrier;
+		
+		if( sh_elapsed_clock->time > OUTPUT_FREQ ) {
+			T_max = pfield_get_max( T );
+			T_min = pfield_get_min( T );
+			if( MYTHREAD == 0 )  {
+				sh_elapsed_clock->time = 0.0;
+				printf("iteration = %d, exec speed [iter/s] = %15.7e, err/tol = %15.7e, T_max = %15.7e, T_min = %15.7e\n", n, (double)n / clock.time, err / TOL, T_max, T_min);
+				fflush(stdout);
 			}
-			upc_barrier;
 		}
 
 	}
 
-	if( MYTHREAD == 0 )
+	if( MYTHREAD == 0 ) {
 		printf("Total wall time = %15.7e s\n", clock.time);
+		fflush(stdout);
+
+		pfield_write_file( T, "T.dat");
+	}
+
+	upc_all_free( sh_elapsed_clock );
 	
 	for( n=0; n<NWORKSP; n++ )
 		pfield_dtor(ws+n);
 
 	pfield_dtor(&D);
-	pfield_dtor(&T);
+	pfield_dtor(T);
+	pfield_dtor(T+1);	
 
+	fini_locks();
+	
 	upc_global_exit;
 
 	return 0;
